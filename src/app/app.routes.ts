bd9e4e33394679cdd71f71bd2f9642d44b627d57
n-zngr <deltagaming.ch.nik@gmail.com>
2025-08-04 15:28:55 +0200

    feat(router): add dashboard, profile routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { App } from './app.component';
+import { Dashboard } from './dashboard/dashboard.component';
+import { Profile } from './profile/profile.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', component: App },
+    { path: 'dashboard', component: Dashboard },
+    { path: 'profile', component: Profile },
+    { path: '**', redirectTo: '' }
+];
